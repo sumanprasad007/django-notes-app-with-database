6ea2040f3003732fc7d31ccd771e2482f412c299
Suman Prasad <55047333+sumanprasad007@users.noreply.github.com>
2023-07-10 17:38:44 +0530

    Create jenkins-installation.sh

---
+++ jenkins-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Import Jenkins GPG key
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add Jenkins repository to apt sources
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package lists
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install jenkins -y
+
+# Display initial admin password
+cat /var/lib/jenkins/secrets/initialAdminPassword
