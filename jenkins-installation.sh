ba5959f7add0c1a40785c279c630818411c814f0
Suman Prasad <55047333+sumanprasad007@users.noreply.github.com>
2023-07-31 13:25:05 +0530

    Update jenkins-installation.sh

--- jenkins-installation.sh
+++ jenkins-installation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Install Java jdk
+sudo apt install openjdk-17-jre
+
 # Import Jenkins GPG key
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
